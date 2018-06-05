17c685c982b1dc39c5862d388be0af5347915b61
Tristan Rivoallan <tristan@rivoallan.net>
2018-06-05 20:30:06 +0200

    [developer-portal] Intégration

---
+++ domain.mk
@@ -0,0 +1,11 @@
+DP_DOCKER_REGISTRY=hub.docker.com
+DP_GIT_PREFIX=git@github.com:constructions-incongrues
+DP_SRV_CODE=daheardit musiqueapproximative musiquesincongrues ouiedire urlinfo vanillaminer
+DP_SRV_SUPORT=solr
+DP_DOCS_DOMAIN=docs.constructions-incongrues.net
+
+incongru-install:
+	#
+
+incongru-uninstall:
+	#
